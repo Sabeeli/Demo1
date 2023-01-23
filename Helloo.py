1831128a077bc48eb716aa1c037c4ae6637df896
unknown <harineesaravanan05@gmail.com>
2023-01-23 11:28:11 +0530

    added

--- Helloo.py
+++ Helloo.py
@@ -2,12 +2,9 @@ print(sabeeli)
 
 print(aksh)
 print(name)
-<<<<<<< HEAD
-print(sneha)
+print(harinee)
 
-=======
 print(madhu)
 print(prathi)
->>>>>>> 54a7889054737489653d76c83dba4b5668201101
 print(sneha)
 ptint(gayac)
