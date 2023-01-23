6d9de3127b378b1fc76b9d5cf9d00af83efb64dd
gaya212001 <gayathrichandran32@gmail.com>
2023-01-23 12:52:41 +0530

    added

--- Helloo.py
+++ Helloo.py
@@ -1,6 +1,11 @@
 //harinee-addition
 //sneha-subtraction
 //gayac-multiplication
+num_1 = 11
+num_2 = 3
+product = num_1 * num_2
+print("product")
+
 //dhivya-division
 //gayat-square
 //sindhu-addition
